ea659e35dd52e6ee56ddaa29c2748384935dfbee
NAPHthalene130 <1301537275@qq.com>
2025-10-28 23:05:37 +0800

    feat:初始化左右状态区

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    QWidget *mainWidget = new QWidget;
+    this->setCentralWidget(mainWidget);
+    this->setFixedSize(1200,800);
+    QWidget *leftQWidget = new QWidget;
+    QWidget *rightWidget = new QWidget;
+    leftQWidget->setStyleSheet("background-color: #E0FFFF; border: 2px solid black;");
+    rightWidget->setStyleSheet("background-color: #F0FFF0; border: 1px solid red;");
+
+    QHBoxLayout *mainHLay = new QHBoxLayout;
+    mainHLay->addWidget(leftQWidget,5);
+    mainHLay->addWidget(rightWidget,1);
+    mainWidget->setLayout(mainHLay);
+
+    //初始化右边组件
+    QVBoxLayout *rightLay = new QVBoxLayout;
+    QLabel *spaceLeftLabel = new QLabel("剩余车位:0");
+
+    rightLay->addWidget(spaceLeftLabel);
+    rightWidget->setLayout(rightLay);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
 }
